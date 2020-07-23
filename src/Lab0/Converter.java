cb657e3b9b2e63dd9191ce4fa0be116b4fb61828
bkcoo <bscoombe@gmail.com>
2020-07-23 10:49:46 -0600

    Added kilometersToMiles

--- src/Lab0/Converter.java
+++ src/Lab0/Converter.java
@@ -17,10 +17,15 @@ public class Converter {
         return (F-32)*5/9;
     }
 
+    public double kilometersToMiles(double kilometers){
+        return kilometers*0.62137;
+    }
+
     public static void main(String [] args){
         Converter c= new Converter();
         System.out.println(c.celsiusToFahrenheit(180));
         System.out.println(c.fahrenheitToCelsius(250));
+        System.out.println(c.kilometersToMiles(30));
 
     }
 
